6a52aaf74a291691a26cce71460bd9013671eab1
XandrRF <xandrnf@gmail.com>
2024-12-25 20:54:21 +0500

    готово

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,20 @@ public class Main {
         System.out.println("Введите любое целое не отрицательное число: ");
         int input = new Scanner(System.in).nextInt();
         System.out.println(avg(input));
+
+
     }
 
     public static double avg(int num) {
         //todo Дописать логику работы метода сюда. Метод main не трогаем!!!
-        return 0; //todo заменить 0 на корректный результат.
+
+        String stringNum = String.valueOf(num);
+        double sum = 0;
+        for (int i = 0; i < stringNum.length(); i++) {
+            int tempNum = Integer.parseInt(String.valueOf(stringNum.charAt(i)));
+            sum = sum + tempNum;
+
+        }
+        return sum / stringNum.length(); //todo заменить 0 на корректный результат.
     }
 }
